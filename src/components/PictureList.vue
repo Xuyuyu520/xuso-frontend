06ab909a5158499c15ec21243433d02bdf518753
风间 <norbocampasjb9657@gmail.com>
2024-05-08 15:51:47 +0800

    前段代码初始化

---
+++ src/components/PictureList.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-list
+    item-layout="horizontal"
+    :grid="{ gutter: 24, column: 3, xs: 8, sm: 16, md: 24 }"
+    :data-source="props.pictureList"
+  >
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-card hoverable>
+          <template #cover>
+            <img alt="example" :src="item.url" />
+          </template>
+          <a-card-meta :title="item.title" />
+        </a-card>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+<script setup lang="ts">
+import { defineProps, withDefaults } from "vue";
+
+interface Props {
+  pictureList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  pictureList: () => [],
+});
+</script>
+
+<style scoped></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchParams.text"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+    <MyDivider />
+    <a-tabs v-model:activeKey="activeKey" @change="onTableChange">
+      <a-tab-pane key="post" tab="文章">
+        <PostList :post-list="postList" />
+      </a-tab-pane>
+      <MyDivider />
+      <a-tab-pane key="picture" tab="图片" force-render>
+        <PictureList :picture-list="pictureList" />
+      </a-tab-pane>
+      <MyDivider />
+      <a-tab-pane key="user" tab="用户">
+        <UserList :user-list="userList" />
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import PostList from "@/components/PostList.vue";
+import PictureList from "@/components/PictureList.vue";
+import UserList from "@/components/UserList.vue";
+import MyDivider from "@/components/MyDivider.vue";
+import { useRoute, useRouter } from "vue-router";
+import myAxios from "@/plugins/myAxios";
+
+const router = useRouter();
+const route = useRoute();
+const initSearchParams = {
+  text: "",
+  pageSize: 10,
+  pageNum: 1,
+};
+const activeKey = route.params.category;
+
+const postList = ref([]);
+const userList = ref([]);
+const pictureList = ref([]);
+/**
+ * 获取数据
+ * @param params
+ */
+const localData = (params: any) => {
+  const postQuery = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("post/list/page/vo", postQuery).then((res: any) => {
+    postList.value = res.records;
+  });
+  const userQuery = {
+    ...params,
+    userName: params.text,
+  };
+  myAxios.post("user/list/page/vo", userQuery).then((res: any) => {
+    userList.value = res.records;
+  });
+  const pictureQuery = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("picture/list/page/vo", pictureQuery).then((res: any) => {
+    pictureList.value = res.records;
+  });
+};
+const searchParams = ref(initSearchParams);
+//首次请求
+localData(initSearchParams);
+
+watchEffect(() => {
+  searchParams.value = {
+    ...initSearchParams,
+    text: route.query.text,
+  } as any;
+});
+
+const onSearch = (value: string) => {
+  console.log(value);
+  router.push({
+    query: searchParams.value,
+  });
+  localData(searchParams.value);
+};
+const onTableChange = (key: string) => {
+  router.push({
+    path: `/${key}`,
+    query: searchParams.value,
+  });
+};
+</script>
